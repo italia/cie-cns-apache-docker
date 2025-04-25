448b8715879da88aa10e6254a61cb07b500fcecf
amusarra <antonio.musarra@gmail.com>
2025-04-25 10:09:20 +0200

    sf: source code formatting

--- scripts/parse-gov-certs.py
+++ scripts/parse-gov-certs.py
@@ -44,26 +44,30 @@ import os
 DEFAULT_XML_URI = "https://eidas.agid.gov.it/TL/TSL-IT.xml"
 EXTENSION = ".pem"
 
-def get_certs_xml():
-  if sys.version_info[0] == 2:
-    import urllib2
-    request = urllib2
-  else:
-    import urllib.request
-    request = urllib.request
 
-  return request.urlopen(DEFAULT_XML_URI)
+def get_certs_xml():
+    if sys.version_info[0] == 2:
+        import urllib2
+        request = urllib2
+    else:
+        import urllib.request
+        request = urllib.request
+
+    return request.urlopen(DEFAULT_XML_URI)
+
 
 def write_certificate(f, x509_cert):
-  f.write('-----BEGIN CERTIFICATE-----\n')
-  for line in textwrap.wrap(x509_cert, 65):
-    f.write(line+'\n')
-  f.write('-----END CERTIFICATE-----\n')
+    f.write('-----BEGIN CERTIFICATE-----\n')
+    for line in textwrap.wrap(x509_cert, 65):
+        f.write(line + '\n')
+    f.write('-----END CERTIFICATE-----\n')
+
 
 def get_service_info(service, namespace):
-  name = service.find("*/"+namespace+"Name").text
-  x509_cert = service.find("*//"+namespace+"X509Certificate").text
-  return {'name': name, 'x509_cert': x509_cert}
+    name = service.find("*/" + namespace + "Name").text
+    x509_cert = service.find("*//" + namespace + "X509Certificate").text
+    return {'name': name, 'x509_cert': x509_cert}
+
 
 def safe_open(file_path, base_path, mode='r'):
     # Get absolute path of the base directory
@@ -83,6 +87,7 @@ def safe_open(file_path, base_path, mode='r'):
     # file deepcode ignore PT: only point to use this function
     return open(full_path, mode)
 
+
 parser = argparse.ArgumentParser()
 action = parser.add_mutually_exclusive_group(required=True)
 action.add_argument("--output-folder", help="Where to save the certs files")
@@ -92,31 +97,30 @@ parser.add_argument("--service-type-identifier", help="Save certs by Service Typ
 args = parser.parse_args()
 
 if args.output_folder:
-  if os.path.exists(args.output_folder):
-    if not os.path.isdir(args.output_folder):
-      print("Impossible to save certificates in `%s': file exists and is not a folder." % args.output_folder)
-      sys.exit(1)
-  else:
-    os.makedirs(args.output_folder)
+    if os.path.exists(args.output_folder):
+        if not os.path.isdir(args.output_folder):
+            print("Impossible to save certificates in `%s': file exists and is not a folder." % args.output_folder)
+            sys.exit(1)
+    else:
+        os.makedirs(args.output_folder)
 elif args.output_file:
-  if os.path.exists(args.output_file):
-    if not os.path.isfile(args.output_file):
-      print("Impossible to write on `%s', it's not a file." % args.output_file)
-      sys.exit(1)
+    if os.path.exists(args.output_file):
+        if not os.path.isfile(args.output_file):
+            print("Impossible to write on `%s', it's not a file." % args.output_file)
+            sys.exit(1)
 
-    print("File `%s' will be overwritten..." % args.output_file)
-
+        print("File `%s' will be overwritten..." % args.output_file)
 
 if args.cert_file:
-  tree = etree.parse(args.cert_file)
-  root = tree.getroot()
+    tree = etree.parse(args.cert_file)
+    root = tree.getroot()
 else:
-  root = etree.fromstring(get_certs_xml().read())
+    root = etree.fromstring(get_certs_xml().read())
 
 try:
-  [default_namespace] = re.findall("({[^}]*}).*", root.tag)
+    [default_namespace] = re.findall("({[^}]*}).*", root.tag)
 except:
-  default_namespace = ""
+    default_namespace = ""
 
 print("Namespace: `%s`", default_namespace)
 
@@ -142,40 +146,40 @@ else:
     services = root.xpath(query, namespaces=ns)
 
 if args.output_folder:
-  for service in services:
-    try:      
-        info = get_service_info(service, default_namespace)
-        name = re.sub('[A-z]{1,2}=', '_', re.sub('[/\,\' "]', '_', info['name'])).replace('__', '_').strip('_- ')
-        filename = name
+    for service in services:
+        try:
+            info = get_service_info(service, default_namespace)
+            name = re.sub('[A-z]{1,2}=', '_', re.sub('[/\,\' "]', '_', info['name'])).replace('__', '_').strip('_- ')
+            filename = name
 
-        idx = 1
-        tmpname = filename
-        while os.path.exists(os.path.join(args.output_folder, tmpname + EXTENSION)):
-            tmpname = filename + str(idx)
-            idx += 1
-        filename = tmpname + EXTENSION
+            idx = 1
+            tmpname = filename
+            while os.path.exists(os.path.join(args.output_folder, tmpname + EXTENSION)):
+                tmpname = filename + str(idx)
+                idx += 1
+            filename = tmpname + EXTENSION
 
-        f = safe_open(filename, args.output_folder, 'w')
-        write_certificate(f, info['x509_cert'])
-        f.close()
-        print("Added certificate: %s" % filename)
+            f = safe_open(filename, args.output_folder, 'w')
+            write_certificate(f, info['x509_cert'])
+            f.close()
+            print("Added certificate: %s" % filename)
 
-    except Exception as e:
-        print("Impossible to add file: %s" % e)
-        pass
+        except Exception as e:
+            print("Impossible to add file: %s" % e)
+            pass
 
 else:
-  f = safe_open(args.output_file, '/', 'w')
+    f = safe_open(args.output_file, '/', 'w')
 
-  for service in services:
-    try:
-      info = get_service_info(service)
-      write_certificate(f, info['x509_cert'])
+    for service in services:
+        try:
+            info = get_service_info(service)
+            write_certificate(f, info['x509_cert'])
 
-      print("Added certificate %s" % info['name'])
+            print("Added certificate %s" % info['name'])
 
-    except Exception as e:
-      print("Impossible to add certificate to file: %s" % e)
-      pass
+        except Exception as e:
+            print("Impossible to add certificate to file: %s" % e)
+            pass
 
-  f.close()
+    f.close()
